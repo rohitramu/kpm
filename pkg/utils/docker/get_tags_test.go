07bd09e1c40ca000b54e60325d444336d3a73132
Rohit Ramu <rohitramu@gmail.com>
2024-11-16 20:02:27 -0800

    Use GitHub Actions to build and publish KPM

--- pkg/utils/docker/get_tags_test.go
+++ pkg/utils/docker/get_tags_test.go
@@ -1,8 +1,10 @@
 package docker
 
 import (
+	"sync"
 	"testing"
 
+	"github.com/rohitramu/kpm/pkg/utils/log"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -14,13 +16,20 @@ func TestGetVersions(t *testing.T) {
 		var ch = make(chan string, 1)
 
 		var tags []string
+		var wg sync.WaitGroup
 		go func() {
+			defer wg.Done()
+			wg.Add(1)
 			for tag := range ch {
 				tags = append(tags, tag)
 			}
 		}()
 
+		log.SetLevel(log.LevelDebug)
+
 		var err = GetImageTags(ch, packageName, dockerRegistry)
+		close(ch)
+		wg.Wait()
 		So(err, ShouldBeNil)
 		So(tags, ShouldContain, "1.0.0")
 	})
